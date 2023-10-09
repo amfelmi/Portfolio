74d592fa290894c09df3dc7d816027b110b12b34
axe208 <axe208@student.bham.ac.uk>
2023-10-09 16:05:06 +0100

    modified:   src/App.scss 	modified:   src/Components/Home/About/index.js 	modified:   src/Components/Home/About/index.scss 	modified:   src/Components/Home/index.scss 	modified:   src/Components/Layout/index.scss 	modified:   src/Components/Sidebar/index.scss

--- src/Components/Home/About/index.js
+++ src/Components/Home/About/index.js
@@ -7,11 +7,22 @@ const About = ({ isActivated }) => {
     <div className={`screen ${isActivated ? 'activated' : 'deactivated'}`}>
       <div className='screen-container'>
         <div className='screen-image-container'>
-      <img  src={Photo} className='photo' />
-      </div>
-      <div className='screen-info-container' >
-      THIS IS ABOUT PAGE!
-      </div>
+          <img src={Photo} className='photo' />
+        </div>
+        <div className='screen-info-container' >
+          <p className='greeting'>
+          <span className='rotate'>👋</span> Hello, I'm Abdulmajid Elmi!
+          </p>
+          <p className='paragraph'>
+            🎓 With a background in <span className='highlight'> Chemical Engineering </span>, I navigated the world of software in the field, sparking my transition to technology.
+          </p>
+          <p className='paragraph'>
+            💻 I earned an <span className='highlight'>MSc in Computer Science </span> from the University of Birmingham. Using <span className='highlight'>Java and React </span>, I'm currently working on my full stack development.
+          </p>
+          <p className='paragraph'>
+            💡 Actively exploring <span className='highlight'>Cloud Computing </span> through Azure, I bring a unique perspective shaped by my experiences in both engineering and technology.
+          </p>
+        </div>
       </div>
     </div>
   )
